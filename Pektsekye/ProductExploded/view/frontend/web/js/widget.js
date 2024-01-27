b45e5ee0bce3b7a1e4d604e7b79c27e7a635384e
NCAT <58818829+leroystrubinsky@users.noreply.github.com>
2024-01-27 12:50:07 -0500

    Add files via upload

---
+++ Pektsekye/ProductExploded/view/frontend/web/js/widget.js
@@ -0,0 +1,11 @@
+
+define([
+    "jquery",    
+    "Pektsekye_ProductExploded/js/main",
+    "jquery-ui-modules/widget"                                   
+],function($, main) {
+  "use strict";
+  
+  $.widget("pektsekye.productExploded", main);
+   
+});  
